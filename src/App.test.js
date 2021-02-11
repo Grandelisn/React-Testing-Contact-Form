f252e7fb5d921fdc0654e50a41febf798e521225
Grandelisn <71465617+Grandelisn@users.noreply.github.com>
2021-02-11 13:08:39 -0500

    mvp y'all (#1)
    * mvp y'all
    
    * Update App.test.js

---
+++ src/App.test.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
+import ContactForm from './components/ContactForm'
+
+test("renders App without crashing", () => {
+  render(<App />);
+});
+
+const breakMyApp = ()=> {
+  throw new Error('Error has been created')
+}
+
+test('input field works', ()=> {
+  render(<ContactForm/>)
+
+  const firstName = screen.getByLabelText(/First Name*/i)
+  const lastName = screen.getByLabelText(/Last Name*/i)
+  const email = screen.getByLabelText(/email*/i)
+  const message = screen.getByLabelText(/message*/i)
+
+  expect(firstName).toBeTruthy()
+  expect(lastName).toBeTruthy()
+  expect(email).toBeTruthy()
+  expect(message).toBeTruthy()
+
+  fireEvent.change(firstName, {target: { value:'Nate'}})
+  fireEvent.change(lastName, {target: { value:'Grandelis'}})
+  fireEvent.change(email, {target: { value:'email@email.com'}})
+  fireEvent.change(message, {target: { value:'random things here'}})
+
+  const form = screen.getByText(/FORM/i)
+  expect(form).toBeTruthy()
+  fireEvent.click(form)
+
+})
